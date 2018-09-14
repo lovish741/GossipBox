b510864e5393a4cb19a52ff0fcb25f0a4f9005cc
lovish <lovishagrawal75@gmail.com>
2018-09-14 08:39:33 +0530

    Login Page and registration page

---
+++ app/src/main/java/com/lovish/areeba/gossipbox/Login_Page.java
@@ -0,0 +1,33 @@
+package com.lovish.areeba.gossipbox;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login_Page extends AppCompatActivity {
+
+    private Button reg_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__page);
+
+        reg_button_submit();
+    }
+
+    public void reg_button_submit()
+    {
+        reg_button= (Button) findViewById(R.id.login_reg_button);
+
+        reg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg_intent= new Intent(Login_Page.this,Reg_activity.class);
+                startActivity(reg_intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/lovish/areeba/gossipbox/Reg_activity.java
@@ -0,0 +1,13 @@
+package com.lovish.areeba.gossipbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Reg_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_activity);
+    }
+}
